e4f6c3b810a3e374f4bde901aceaede070a100e9
Gijs Bartholomeus <gijsbartholomeus@gijss-air.home>
2022-03-30 11:12:53 +0200

    edit

--- entropysolver.py
+++ entropysolver.py
@@ -11,3 +11,16 @@ from scipy import linalg as LA
 from random import gauss
 from math import sqrt
 import matplotlib.pylab as plt
+
+"Generate random symmetric matrix"
+n=20
+for k in range(n):                         # dimension of the random matrix
+    G = np.zeros((n,n))             # initialize a two-dimensional array
+    for i in range(n):
+        for j in range(i,n):
+            G[i, j] = gauss(0,1)    # random element
+            G[j, i] = G[i, j]   
+
+"Generate random species vector, and normalize species abundance"
+
+"We will solve for N_i(K-N_i) "
